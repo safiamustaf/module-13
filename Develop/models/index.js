554ed4fa89515e52d06de455753a30207fd134dd
FIRST_NAME LAST_NAME <safiamustaf>
2023-05-30 19:59:37 -0500

    first

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -9,10 +9,12 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
@@ -20,6 +22,10 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
